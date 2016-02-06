677eefa07df3a29498063b9f8f8ded397e32ccc9
wata_mac <watassbass@gmail.com>
2016-02-06 21:48:00 +0900

    fix style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-      render text: "hola, mundo!"
+    render text: 'hola, mundo!'
   end
 end
