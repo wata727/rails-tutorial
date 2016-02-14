94b872b1ea6da8f27c96e188771663004fec847c
wata_mac <watassbass@gmail.com>
2016-02-14 22:57:42 +0900

    create user validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+              format: { with: VALID_EMAIL_REGEXP },
+              uniqueness: { case_sensitive: false }
 end
